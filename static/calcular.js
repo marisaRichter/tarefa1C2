cde15a7a9f82efc39ced1f0829d56c4dd7f1c386
marisaRichter <marisa.richter@compasso.com.br>
2020-04-27 21:46:55 -0300

    first commit

---
+++ static/calcular.js
@@ -0,0 +1,20 @@
+function calcular(){
+	var n = $("#rets").val();
+	var pt = 17 * 4;
+	console.log("PT: " + pt + " - n " + n);
+	var c = new Array();
+	var f = new Array();
+	var a = 0;
+	var dx = 3/n;
+	var html = "<tr>"+
+					"<th>" + n + "</th>";
+	
+	for(var i = 1; i <= n; i++){
+		c[i] = (i - 1 + (pt/100))*(dx);
+		f[i] = 3-(Math.pow(c[i],2))/3;
+		a = a + (f[i] * dx);
+	}
+	html += "<th>" + a + "</th></tr>";
+	$(".table tbody").append(html);
+	console.log("A = " + a);
+}
